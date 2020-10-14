96b64ed915dcfb7ba3283d47e68000b5abd624e6
ac97nj <ac97nj@gmail.com>
2020-10-14 09:01:55 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <HelloWorld></HelloWorld>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
